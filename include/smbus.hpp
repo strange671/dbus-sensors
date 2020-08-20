cad3f0ced09e087e379129c8d41515755994769c
Jet Li <Jet.Li@ibm.com>
2020-08-21 01:57:57 +0800

    Add the smbus related and send raw cmd functions.
    Signed-off-by: Jet Li <Jet.Li@ibm.com>

--- include/smbus.hpp
+++ include/smbus.hpp
@@ -19,21 +19,11 @@ class Smbus
   public:
     Smbus(){};
 
-    int openI2cDev(int i2cbus, char* filename, size_t size, int quiet);
-
-    int set_slave_addr(int file, int address, int force);
+    int open_i2c_dev(int i2cbus, char* filename, size_t size, int quiet);
 
     int smbusInit(int smbus_num);
 
     void smbusClose(int smbus_num);
-
-    int SetSmbusCmdByte(int smbus_num, int8_t device_addr, int8_t smbuscmd , int8_t data);
- 
-    int GetSmbusCmdByte(int smbus_num, int8_t device_addr, int8_t smbuscmd);
-
-    int SendSmbusRWBlockCmdRAW(int smbus_num, int8_t device_addr,
-                               uint8_t* tx_data, uint8_t tx_len,
-                               uint8_t* rsp_data);
 
 };
 
--- src/NVMeSensor.cpp
+++ src/NVMeSensor.cpp
@@ -18,7 +18,8 @@
 
 #include "NVMeDevice.hpp"
 
-#include "i2c/smbus.h"
+#include "smbus.hpp"
+#include "i2c.h"
 
 #include <crc32c.h>
 #include <libmctp-smbus.h>
@@ -28,6 +29,8 @@
 
 #include <iostream>
 #include <mutex>
+
+#define MAX_I2C_BUS 30
 
 static constexpr double maxReading = 127;
 static constexpr double minReading = 0;
@@ -117,6 +120,106 @@ void init()
 }
 
 } // namespace nvmeMCTP
+
+namespace phosphor
+{
+namespace smbus
+{
+
+std::mutex gMutex;
+
+int phosphor::smbus::Smbus::open_i2c_dev(int i2cbus, char *filename, size_t size, int quiet)
+{
+    int file;
+
+    snprintf(filename, size, "/dev/i2c/%d", i2cbus);
+    filename[size - 1] = '\0';
+    file = open(filename, O_RDWR);
+
+    if (file < 0 && (errno == ENOENT || errno == ENOTDIR)) {
+        sprintf(filename, "/dev/i2c-%d", i2cbus);
+        file = open(filename, O_RDWR);
+    }
+
+    if (file < 0 && !quiet) {
+        if (errno == ENOENT) {
+            fprintf(stderr, "Error: Could not open file "
+                "`/dev/i2c-%d' or `/dev/i2c/%d': %s\n",
+                i2cbus, i2cbus, strerror(ENOENT));
+        } else {
+            fprintf(stderr, "Error: Could not open file "
+                "`%s': %s\n", filename, strerror(errno));
+            if (errno == EACCES)
+                fprintf(stderr, "Run as root?\n");
+        }
+    }
+
+    return file;
+}
+
+int phosphor::smbus::Smbus::smbusInit(int smbus_num)
+{
+    int res = 0;
+    char filename[20];
+    int fd[MAX_I2C_BUS] = {0};
+
+    gMutex.lock();
+
+    fd[smbus_num] = open_i2c_dev(smbus_num, filename, sizeof(filename), 0);
+    if (fd[smbus_num] < 0)
+    {
+        gMutex.unlock();
+
+        return -1;
+    }
+
+    res = fd[smbus_num];
+
+    gMutex.unlock();
+
+    return res;
+}
+
+void phosphor::smbus::Smbus::smbusClose(int smbus_num)
+{
+    int fd[MAX_I2C_BUS] = {0};
+
+    close(fd[smbus_num]);
+}
+
+int phosphor::smbus::Smbus::SendSmbusRWBlockCmdRAW(int smbus_num,
+                                                   int8_t device_addr,
+                                                   uint8_t* tx_data,
+                                                   uint8_t tx_len,
+                                                   uint8_t* rsp_data)
+{
+    int res, res_len;
+    unsigned char Rx_buf[I2C_DATA_MAX] = {0};
+
+    Rx_buf[0] = 1;
+
+    gMutex.lock();
+
+    res = i2c_read_after_write(fd[smbus_num], device_addr, tx_len,
+                               (unsigned char*)tx_data, I2C_DATA_MAX,
+                               (unsigned char*)Rx_buf);
+
+    if (res < 0)
+    {
+        fprintf(stderr, "Error: SendSmbusRWBlockCmdRAW failed\n");
+    }
+
+    res_len = Rx_buf[0] + 1;
+
+    memcpy(rsp_data, Rx_buf, res_len);
+
+    gMutex.unlock();
+
+    return res;
+}
+
+} // namespcae smbus
+} // namespace phosphor
 
 static int lastQueriedDeviceIndex = -1;
 
