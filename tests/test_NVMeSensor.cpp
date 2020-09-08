cbe2f1a7c4595f37e72be1fd23036853437e9fc3
Jet Lee <mygoodjet@gmail.com>
2020-09-09 00:42:19 +0800

    add getBusID testcase
    Signed-off-by: Jet Li <Jet.Li@ibm.com>

---
+++ tests/test_NVMeSensor.cpp
@@ -0,0 +1,12 @@
+#include <NVMeSensor.hpp>
+#include <dbus/connection.hpp>
+#include <nlohmann/json.hpp>
+
+#include <fstream>
+
+#include "gtest/gtest.h"
+
+TEST(getNVMeInfobyBusID, TestgetNVMeInfobyBusID)
+{
+
+}
