b2f4ca5c4f1662c024ec24652471df3e85380636
Jet Li <Jet.Li@ibm.com>
2020-09-09 17:10:10 +0800

    add testcase bool function
    Signed-off-by: Jet Li <Jet.Li@ibm.com>

--- tests/test_NVMeSensor.cpp
+++ tests/test_NVMeSensor.cpp
@@ -2,11 +2,29 @@
 #include <dbus/connection.hpp>
 #include <nlohmann/json.hpp>
 
+#include "util.hpp"
+
+#include <cstring>
+#include <stdexcept>
+#include <stdplus/raw.hpp>
+#include <string>
+#include <vector>
 #include <fstream>
 
 #include "gtest/gtest.h"
 
+namespace phosphor
+{
+namespace nvme
+{
+
 TEST(getNVMeInfobyBusID, TestgetNVMeInfobyBusID)
 {
 
+    bool getData = getNVMeInfobyBusID(int busID, phosphor::nvme::Nvme::NVMeData& nvmeData);
+    EXPECT_THROW(getData, std::runtime_error);
+    EXPECT_EQ(true, getData);
 }
+
+} // namespace nvme
+} // namespace phosphor
